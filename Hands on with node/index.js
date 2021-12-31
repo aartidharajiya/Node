9eba5c8370c6e7470c206b8c2d57aa0a85a0ba55
Yameen <mmacchhar949@rku.ac.in>
2021-12-31 09:31:17 +0530

    Fun Added

--- Hands on with node/index.js
+++ Hands on with node/index.js
@@ -2,6 +2,14 @@ var sum = (a,b)=>{
     return a+b;
 }
 
+var mul = (a,b)=>{
+    return a*b;
+}
+
+var div = (a,b)=>{
+    return a/b;
+}
+
 var name = 'RKU';
 
-module.exports = {sum,name}
+module.exports = {sum,mul,div,name}
